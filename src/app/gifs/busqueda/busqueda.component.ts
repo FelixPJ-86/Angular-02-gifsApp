a9118c3bea497f9b96ed7698e9cdf0002db94ad1
Félix <felix.potente@alten.es>
2021-06-03 13:20:55 +0200

    fin tema7 video 8

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+@ViewChild('txtBuscar') txtBuscar:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
-  }
-
+buscar(){
+ const valor =this.txtBuscar.nativeElement.value;
+ console.log(valor);
+ this.txtBuscar.nativeElement.value='';
+}
 }
